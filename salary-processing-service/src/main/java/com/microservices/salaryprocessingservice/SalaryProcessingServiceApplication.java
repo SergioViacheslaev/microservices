2b791cf3cecde74ecd761ae475e59bf7cdc7a7a4
SergioViacheslaev <internetwalker@yandex.ru>
2023-05-07 20:07:28 +0300

    Add salary processing service

---
+++ salary-processing-service/src/main/java/com/microservices/salaryprocessingservice/SalaryProcessingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.salaryprocessingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaryProcessingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalaryProcessingServiceApplication.class, args);
+    }
+
+}
---
+++ salary-processing-service/src/test/java/com/microservices/salaryprocessingservice/SalaryProcessingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.salaryprocessingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaryProcessingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
