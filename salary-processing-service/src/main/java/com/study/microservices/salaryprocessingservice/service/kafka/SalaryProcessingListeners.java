0a43c620b90998d7544fac307794fbd272d98fda
SergioViacheslaev <internetwalker@yandex.ru>
2023-08-08 19:32:18 +0300

    Disable kafka auto commit, listener manual ack

--- salary-processing-service/src/main/java/com/study/microservices/salaryprocessingservice/service/kafka/SalaryProcessingListeners.java
+++ salary-processing-service/src/main/java/com/study/microservices/salaryprocessingservice/service/kafka/SalaryProcessingListeners.java
@@ -6,6 +6,7 @@ import static com.study.microservices.salaryprocessingservice.config.KafkaConfig
 import com.study.microservices.salaryprocessingservice.model.dto.EmployeePaymentDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -18,10 +19,12 @@ public class SalaryProcessingListeners {
     @KafkaListener(topics = SALARY_PAYMENT_TOPIC, groupId = SALARY_PAYMENT_CONSUMER_GROUP)
     public void consume(
             @Payload EmployeePaymentDto employeePayment,
-            @Header(value = KafkaHeaders.RECEIVED_KEY, required = false) String key
+            @Header(value = KafkaHeaders.RECEIVED_KEY, required = false) String key,
+            Acknowledgment ack
     ) {
         log.info("Consumed key: {}", key);
         log.info("Consumed employeePayment: {}", employeePayment);
+        ack.acknowledge();
     }
 
 }
