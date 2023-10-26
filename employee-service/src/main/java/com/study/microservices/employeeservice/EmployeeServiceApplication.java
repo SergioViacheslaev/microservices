161de1f2366fd0fc059e6d55e568e592928d85da
SergioViacheslaev <internetwalker@yandex.ru>
2023-10-26 22:27:53 +0300

    Update dependencies versions

--- employee-service/src/main/java/com/study/microservices/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/com/study/microservices/employeeservice/EmployeeServiceApplication.java
@@ -1,15 +1,18 @@
 package com.study.microservices.employeeservice;
 
 import com.study.microservices.employeeservice.util.KotlinUtilsKt;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class EmployeeServiceApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
+        log.info("Hello from Java {}", System.getProperty("java.version"));
         KotlinUtilsKt.printKotlinCurrentVersion();
     }
 
