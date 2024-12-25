e2eca86528c7ac4ee8429bd4a2246851b2c7b5db
SergioViacheslaev <internetwalker@yandex.ru>
2024-12-25 13:03:26 +0300

    Remove tag

--- employee-service/src/main/java/com/study/microservices/employeeservice/async/controller/AsyncEmployeeUploadController.java
+++ employee-service/src/main/java/com/study/microservices/employeeservice/async/controller/AsyncEmployeeUploadController.java
@@ -1,26 +1,20 @@
 package com.study.microservices.employeeservice.async.controller;
 
-import com.study.microservices.employeeservice.exception.InvalidCsvException;
 import com.study.microservices.employeeservice.async.service.AsyncEmployeeUploadService;
-import com.study.microservices.employeeservice.async.service.TicketManager;
 import com.study.microservices.employeeservice.async.service.TempFileService;
+import com.study.microservices.employeeservice.async.service.TicketManager;
+import com.study.microservices.employeeservice.exception.InvalidCsvException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.ErrorResponse;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
@@ -36,7 +30,6 @@ import static com.study.microservices.employeeservice.util.constants.CsvConstant
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/async/employee")
-@Tag(name = "Async Employee CSV file upload controller")
 public class AsyncEmployeeUploadController {
 
     private final TicketManager ticketManager;
